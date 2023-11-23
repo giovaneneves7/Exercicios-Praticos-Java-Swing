d738181aa860d05cc971b8bcb20b3636863cfbe3
Giovane Neves <123518676+giovaneneves7@users.noreply.github.com>
2023-11-23 14:16:55 -0300

    Add files via upload

---
+++ musicplayer/src/main/java/com/github/giovaneneves7/musicplayer/Musicplayer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.giovaneneves7.musicplayer;
+
+/**
+ *
+ * @author curso
+ */
+public class Musicplayer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
